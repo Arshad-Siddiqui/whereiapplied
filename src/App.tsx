5e563a0f0667eb1c09172fcee19564a98c95e415
Arshad Siddiqui <ferozarshad123@gmail.com>
2023-06-01 21:37:17 +0100

    Project setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <p>Hello this is an app that is even bigger now yaayayyayayaya</p>
+    </div>
+  );
+}
+
+export default App;
