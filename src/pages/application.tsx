8063fd21dd5dd78cee6fe061177ac9ac6912c072
Arshad Siddiqui <ferozarshad123@gmail.com>
2023-06-21 22:12:50 +0100

    Make home button responsive

--- src/pages/application.tsx
+++ src/pages/application.tsx
@@ -1,12 +1,13 @@
 import { Button } from "@mui/material";
 import ApplicationForm from "../components/applicationForm";
 import "./application.css";
+import { Link } from "react-router-dom";
 
 export default function ApplicationPage() {
   return (
     <div className="application-form-page page">
       <ApplicationForm />
-      <Button variant="contained" href="/" color="primary">
+      <Button variant="contained" component={Link} to="/" color="primary">
         Home
       </Button>
     </div>
