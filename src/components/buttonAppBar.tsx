c144cc8a31d1d21d576f5081fdfacce257b5127b
Arshad Siddiqui <ferozarshad123@gmail.com>
2023-06-21 19:49:29 +0100

    Add sort button to nav bar

--- src/components/buttonAppBar.tsx
+++ src/components/buttonAppBar.tsx
@@ -6,8 +6,13 @@ import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import Add from "@mui/icons-material/Add";
+import { Link } from "react-router-dom";
 
 export default function ButtonAppBar() {
+  const sort = () => {
+    console.log("sort");
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -24,8 +29,11 @@ export default function ButtonAppBar() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Feed
           </Typography>
-          <Button color="inherit">
+          <Button color="inherit" component={Link} to="/application-form">
             <Add color="inherit" />
+          </Button>
+          <Button color="inherit" onClick={sort}>
+            Sort
           </Button>
         </Toolbar>
       </AppBar>
