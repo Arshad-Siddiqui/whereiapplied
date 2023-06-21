d6e2eff4ed076fe72e18c95e9e3c9cdcc9f4a5a1
Arshad Siddiqui <ferozarshad123@gmail.com>
2023-06-21 17:46:21 +0100

    Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import FeedPage from "./pages/feed";
 
 function App() {
   return (
-    <div className="app">
-      <FeedPage />
-    </div>
+    <Routes>
+      <Route path="/" element={<FeedPage />} />
+      <Route path="*" element={<h1>404</h1>} />
+    </Routes>
   );
 }
 
