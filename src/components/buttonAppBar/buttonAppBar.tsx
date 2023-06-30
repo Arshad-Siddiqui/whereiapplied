a72edc1f98ea0a6983b1a0a0662d1143adc3ab6d
Arshad Siddiqui <ferozarshad123@gmail.com>
2023-06-30 16:18:44 +0100

    Update ButtonAppBar imports

--- src/components/buttonAppBar/buttonAppBar.tsx
+++ src/components/buttonAppBar/buttonAppBar.tsx
@@ -1,9 +1,11 @@
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
+import {
+  Toolbar,
+  Typography,
+  Button,
+  Box,
+  AppBar,
+  IconButton,
+} from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import Add from "@mui/icons-material/Add";
 import { Link } from "react-router-dom";
