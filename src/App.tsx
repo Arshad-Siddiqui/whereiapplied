e465fd01e3991118b217f4fd5bd47ab0cedbe682
Arshad Siddiqui <ferozarshad123@gmail.com>
2023-06-30 11:25:38 +0100

    Move the useEffect to app

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,40 @@ import "./App.css";
 import FeedPage from "./pages/feed";
 import ApplicationPage from "./pages/application";
 import Test from "./pages/test";
+import { Application } from "./types";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [applications, setApplications] = useState<Application[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    fetch("https://whereiapplied.onrender.com/applications", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      mode: "cors",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setApplications(data.reverse());
+        setIsLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <Routes>
-      <Route path="/" element={<FeedPage />} />
+      <Route
+        path="/"
+        element={
+          <FeedPage
+            applications={applications}
+            setApplications={setApplications}
+            isLoading={isLoading}
+          />
+        }
+      />
       <Route path="/application-form" element={<ApplicationPage />} />
       <Route path="/test" element={<Test />} />
       <Route path="*" element={<h1>404</h1>} />
--- src/pages/feed.tsx
+++ src/pages/feed.tsx
@@ -1,29 +1,18 @@
 import ButtonAppBar from "../components/buttonAppBar/buttonAppBar";
 import Feed from "../components/feed/feed";
 import Footer from "../components/footer/footer";
-import { useEffect, useState } from "react";
+import { Dispatch, SetStateAction } from "react";
 import { Application } from "../types";
 
-export default function FeedPage() {
-  const [applications, setApplications] = useState<Application[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-
-  useEffect(() => {
-    fetch("https://whereiapplied.onrender.com/applications", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      mode: "cors",
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        setApplications(data.reverse());
-        setIsLoading(false);
-      })
-      .catch((err) => console.log(err));
-  }, []);
-
+export default function FeedPage({
+  applications,
+  isLoading,
+  setApplications,
+}: {
+  applications: Application[];
+  isLoading: boolean;
+  setApplications: Dispatch<SetStateAction<Application[]>>;
+}) {
   return (
     <div className="feed-page page">
       <ButtonAppBar setApplications={setApplications} />
